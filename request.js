f161e83912c404d6548df53b291d8e5c6d54f826
josue guzman <josueguzman2698@gmail.com>
2016-10-31 23:26:54 -0400

    added stages 2-4 to the code

--- request.js
+++ request.js
@@ -1,3 +1,12 @@
+//helper function
+function simplePost( myUrl, myData, myFunc){
+  $.ajax({
+    type: "POST",
+    url: myUrl,
+    data: myData,
+    complete: myFunc
+  });
+}
 
 // The following will call a POST call to register for code2040
 function register(){
@@ -6,36 +15,24 @@ function register(){
     "token" : "391d758b4e5cf621c44f4f0410695886",
     "github" : "https://github.com/josueguzman2698/code2040project"
   };
-  $.ajax({
-    type: "POST",
-    url: myUrl,
-    data: myData,
-  });
+  simplePost( myUrl, myData, function(){});
 }
 
 //this will recieve a string which needs to be reversed
-function getString(){
-  var myUrl ="http://challenge.code2040.org/api/reverse"
+function getReverse(){
+  //set up necessary variables
+  var myUrl ="http://challenge.code2040.org/api/reverse";
   var myData={
     "token" : "391d758b4e5cf621c44f4f0410695886"
   };
-  $.ajax({
-    type: "POST",
-    url: myUrl,
-    data: myData,
-    complete: function(r) {
-        var text = r.responseText;
-        console.log(text);
-        var backwards = reverseText(text);
-        console.log(backwards);
-        sendReversedString(reverseText(text));
-    }
+  simplePost( myUrl, myData, function(r){
+    sendReversedString(reverseText(r.responseText));
   });
 }
 
 function reverseText(str){
-  var reversed = "";
-  var length = str.length - 1;
+  var reversed = "",
+      length = str.length - 1;
   for(var i = length; i >= 0; i--){
     reversed = reversed + str[i];
   }
@@ -43,57 +40,85 @@ function reverseText(str){
 }
 
 function sendReversedString(datum){
-  var myUrl = "http://challenge.code2040.org/api/reverse/validate"
+  var myUrl = "http://challenge.code2040.org/api/reverse/validate";
   var myData = {
     "token" : "391d758b4e5cf621c44f4f0410695886",
     "string" : datum
   };
-  $.ajax({
-    type: "POST",
-    url: myUrl,
-    data: myData,
-    complete: function(r){}
-  });
+  simplePost( myUrl, myData, function(){});
 }
 
-function findNeedle(){
-  var myUrl = "http://challenge.code2040.org/api/haystack"
+function getNeedleAndHaystack(){
+  var myUrl = "http://challenge.code2040.org/api/haystack";
   var myData = {
     "token" : "391d758b4e5cf621c44f4f0410695886",
   }
-  $.ajax({
-    type: "POST",
-    url: myUrl,
-    data: myData,
-    complete: function(r){
-      var parsedR = JSON.parse(r.responseText);
-      var needleAndHaystack = parsedR;
-      console.log(needleAndHaystack);
-      var need = needleAndHaystack["needle"];
-      var hay = needleAndHaystack["haystack"];
-      console.log(need, hay);
-      var location = locationInHaystack(need, hay);
-      console.log(location);
+  simplePost( myUrl, myData,   function (r){
+      var needleAndHaystack = JSON.parse(r.responseText),
+          need = needleAndHaystack["needle"],
+          hay = needleAndHaystack["haystack"],
+          location = findLocationInHaystack(need, hay);
       sendLocation(location);
-    }
-  });
+    });
 }
-function locationInHaystack(need, hay){
+
+function findLocationInHaystack(need, hay){
   for ( i=0; i<hay.length; i++){
-    if(hay[i] == need){
+    if(hay[i] == need)
       return i;
-    }
   }
 }
+
 function sendLocation(loc){
   var myUrl = "http://challenge.code2040.org/api/haystack/validate";
   var myData = {
     "token" : "391d758b4e5cf621c44f4f0410695886",
     "needle" : loc
   };
-  $.ajax({
-    type: "POST",
-    url: myUrl,
-    data: myData,
+  simplePost( myUrl, myData, function(){});
+}
+
+function getPrefix(){
+  var myUrl = "http://challenge.code2040.org/api/prefix";
+  var myData = {
+    "token" : "391d758b4e5cf621c44f4f0410695886",
+  }
+  simplePost( myUrl, myData, function(r) {
+    var response = JSON.parse(r.responseText),
+        prefix = response.prefix,
+        arr = response.array,
+        arrayWithRemovedPrefixes = removePrefixes(prefix, arr);
+    sendPrefix(arrayWithRemovedPrefixes);
+  });
+}
+
+function removePrefixes(pre, arr){
+  var modifiedArray = [];
+  for( var i = 0; i < arr.length; i++){
+    if (arr[i].length < pre.length)
+      modifiedArray.push(arr[i]);
+    else if (!(pre == arr[i].substring(0, pre.length)))
+      modifiedArray.push(arr[i]);
+  }
+  return modifiedArray;
+}
+
+function sendPrefix(arr){
+  var myUrl = "http://challenge.code2040.org/api/prefix/validate";
+  var myData = {
+    "token" : "391d758b4e5cf621c44f4f0410695886",
+    "array" : arr
+  };
+  simplePost( myUrl, myData, function(){});
+}
+
+function getDateStamp(){
+  var myUrl = "http://challenge.code2040.org/api/dating";
+  var myData = {
+    "token" : "391d758b4e5cf621c44f4f0410695886"
+  };
+  simplePost( myUrl, myData, function(r){
+    var response = JSON.parse(r.responseText);
+    console.log(response);
   });
 }
